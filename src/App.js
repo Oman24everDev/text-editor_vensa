e6387dc781a9a1165129ada0839c4a1218b020dc
Oman24everDev <oman24ever@gmail.com>
2023-03-02 20:41:08 +0800

    fetch api & separate the key

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { API_KEY, API_URL } from './api';
 import './App.css';
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
   const [textValue, setText] = useState(null)
 
   useEffect(() => {
-    fetch(`https://frontendtest.azurewebsites.net/Template/{bf3c866c-9593-4799-9dda-90bac7996913}`)
+    fetch(`${API_URL}/${API_KEY}`)
     .then(async (response) => response.json())
     .then(res => console.log(res))
     .catch(err => console.log(err)
---
+++ src/api.js
@@ -0,0 +1,2 @@
+export const API_URL = "https://frontendtest.azurewebsites.net/Template"
+export const API_KEY = "bf3c866c-9593-4799-9dda-90bac7996913"
