5a3d4e624add2a602529c0a71d50b86078044414
Oman24everDev <oman24ever@gmail.com>
2023-03-02 20:37:35 +0800

    import useState & useEffect, fetch api & logged to console

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [textValue, setText] = useState(null)
+
+  useEffect(() => {
+    fetch(`https://frontendtest.azurewebsites.net/Template/{bf3c866c-9593-4799-9dda-90bac7996913}`)
+    .then(async (response) => response.json())
+    .then(res => console.log(res))
+    .catch(err => console.log(err)
+    )
+  }, [])  
+
   return (
     <div className="App">
       <h1>Hello!</h1>
