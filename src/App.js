7f66a5fceed5b7c8eac1ef79c54a044b10207d0a
Oman24everDev <oman24ever@gmail.com>
2023-03-03 14:40:18 +0800

    added useEffect & character counts (pending)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 function App() {
 
   const [data, setText] = useState("")
+  const [charCount, setCharCount] = useState(0);
+
 
   const handleButtonClick = () => {
     fetch(`${API_URL}/${API_KEY}`)
@@ -15,20 +17,33 @@ function App() {
       });
   }
 
+
+  useEffect(() => {
+    const stringData = JSON.stringify(`${data}`);
+    setCharCount(stringData.length);
+  }, [data]);
+
+
   const handleSave = () => {
     console.log(`Saving template: ${data.template}`);
-    setText('');
   };
 
   const handleChange = (event) => {
-    setText(event.target.value);
-  }
+    const newText = event.target.value;
+    setText(newText.slice(0, 160));
+    setCharCount(newText.length);
+  };
 
-  
 
   return (
     <div className="App">
-      <textarea name="" id="textfield" cols="65" rows="10" value={data.template} onChange={handleChange} ></textarea>
+      <textarea 
+        id="textfield" cols="65" rows="10"
+        value={data.template}
+        onChange={handleChange}
+        style={{ color: charCount > 160 ? "red" : "gray" }}>   
+      </textarea>
+      <span>{charCount} of 160 characters</span>
       <div>
         <button className='generate-btn' onClick={handleButtonClick}>Generate</button>
         <button className='save-btn' onClick={handleSave}>Save</button>
